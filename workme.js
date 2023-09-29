2715a08542cf119c902be0b91cee0c3bb020b593
DrPanayioths <panayioths0072019@gmail.com>
2023-09-29 22:36:51 +0300

    Update: Random Country System Added
    Update and add the JS so the HTDS Button Work

---
+++ workme.js
@@ -0,0 +1,44 @@
+
+
+
+function main() {
+
+
+
+const linko = [
+    [1],
+    [2],
+    [3],
+    [4],
+    [5],
+    [6],
+    [7],
+    [8],
+    [9],
+    [10],
+    [11],
+    [12],
+    [13],
+    [14],
+    [15],
+    [16],
+    [17],
+    [18],
+    [19],
+    [20]
+  ];
+
+
+var numb = (Math.floor(Math.random() * 20) + 1)
+
+
+
+var result = linko[numb];
+
+console.log(result);
+
+
+window.location.href = "https://cptsnake.github.io/GuessTheDS/" + result + ".html";
+}
+
+
