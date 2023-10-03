8e417a62fcb7553ceb00db067507de6590195204
DrPanayioths <panayioths0072019@gmail.com>
2023-10-03 17:48:45 +0300

    Update: Added Correct System (BETA)

--- game.js
+++ game.js
@@ -1,17 +1,29 @@
 // Correct JS
-
+function rul() {
+    corrects()
+    corl()
+    ful()
+}
 
 
 function corrects() {
     var cortext = document.getElementById('cortaxt');
     cortext.style.visibility='visible'
+    
 
-    console.log("roblox")
+
 };
 
 
 
+function corl() {
+   var corboxs = document.getElementById('corboxs')
+   corboxs.style.visibility='visible'
+}
 
 
 
-
+function ful() {
+    var gonl = document.getElementById('gonext')
+    gonext.style.visibility='visible'
+}
